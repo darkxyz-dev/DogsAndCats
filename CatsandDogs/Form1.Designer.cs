2646235dca8175b477c00a148b13e57e315f15bc
Opilane TTHK <opilane@tthk.ee>
2021-01-25 13:57:49 +0200

    first commit

---
+++ CatsandDogs/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CatsandDogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dogPicture = new System.Windows.Forms.PictureBox();
+            this.GetADog = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dogPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dogPicture
+            // 
+            this.dogPicture.Location = new System.Drawing.Point(621, 47);
+            this.dogPicture.Name = "dogPicture";
+            this.dogPicture.Size = new System.Drawing.Size(150, 196);
+            this.dogPicture.TabIndex = 0;
+            this.dogPicture.TabStop = false;
+            this.dogPicture.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // GetADog
+            // 
+            this.GetADog.Location = new System.Drawing.Point(621, 313);
+            this.GetADog.Name = "GetADog";
+            this.GetADog.Size = new System.Drawing.Size(150, 79);
+            this.GetADog.TabIndex = 1;
+            this.GetADog.Text = "button1";
+            this.GetADog.UseVisualStyleBackColor = true;
+            this.GetADog.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GetADog);
+            this.Controls.Add(this.dogPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dogPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox dogPicture;
+        private System.Windows.Forms.Button GetADog;
+    }
+}
+
---
+++ CatsandDogs/Form1.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatsandDogs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dogImage = GetDogImageURL();
+
+            dogPicture.ImageLocation = dogImage;
+            dogPicture.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        public static string GetDogImageURL()
+        {
+            string url = "https://dog.ceo/api/breeds/image/random";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            string imageUrl;
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                Dog dog = JsonConvert.DeserializeObject<Dog>(response);
+
+                imageUrl = dog.message;
+            }
+            return imageUrl;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
